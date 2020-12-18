218fae1e5c9c381f543d9b910af51ba3eb7307a2
unknown <juan.bueno01@correo.usa.edu.co>
2020-12-18 18:09:05 -0500

    se agrego las siguientes clases: usuario, direccion, departamento, ciudad, pago domiciliario y empaque

--- dao/models.py
+++ dao/models.py
@@ -7,8 +7,9 @@ class Persona:
     - número y tipo de documento
     - teléfono
     - correo
+    - Direccion
     """
-    def __init__(self, persona_ID,primerNombre,segundoNombre,primerApellido,segundoApellido,tipoDocumento,documento,telefono,correo):
+    def __init__(self, persona_ID,primerNombre,segundoNombre,primerApellido,segundoApellido,tipoDocumento,documento,telefono,correo,direccion):
         self.persona_ID=persona_ID
         self.primerNombre=primerNombre
         self.segundoNombre=segundoNombre
@@ -18,22 +19,133 @@ class Persona:
         self.documento=documento
         self.telefono=telefono
         self.correo=correo
+        self.direccion
 class Cliente(Persona):
+    """
+    Clase Cliente 
+    Parámetros:
+    - Id del cliente 
+    - Id de persona
+    - Nombres y apelllidos
+    - número y tipo de documento
+    - teléfono
+    - correo
+    - Direccion
+    """
     def __init__(self,cliente_ID,persona_ID,primerNombre,segundoNombre,primerApellido,segundoApellido,tipoDocumento,documento,telefono,correo,tipoCliente):
         self.cliente_ID=cliente_ID
         self.tipoCliente=tipoCliente
         Persona.__init__(self,persona_ID,primerNombre,segundoNombre,primerApellido,segundoApellido,tipoDocumento,documento,telefono,correo)
-
+class Usuario(persona):
+    """
+    Clase Usuario 
+    Parámetros:
+    - Id del usuario 
+    - Id de persona
+    - Nombres y apelllidos
+    - número y tipo de documento
+    - teléfono
+    - correo
+    - Direccion
+    - contraseña
+    - Id del rol
+    """
+    def __init__(self,usuario_ID,persona_ID,primerNombre,segundoNombre,primerApellido,segundoApellido,tipoDocumento,documento,telefono,correo, contraseña, rol_ID):
+        self.usuario_ID=usuario_ID
+        self.contraseña=contraseña
+        self.rol_ID=rol_ID
+        Persona.__init__(self,persona_ID,primerNombre,segundoNombre,primerApellido,segundoApellido,tipoDocumento,documento,telefono,correo)
+class Direccion():
+    """
+    Clase direccion 
+    Parámetros:
+    - Id de la direccion 
+    - Id de la ciudad 
+    - id del departamento
+    - barrio
+    """
+    def __init__(self,direccion_ID,ciudad_ID,departamento_ID,barrio):
+        self.direccion_ID=direccion_ID
+        self.ciudad_ID=ciudad_ID
+        self.departamento_ID=departamento_ID
+        self.barrio=barrio
+class Departamento():
+    """
+    Clase direccion 
+    Parámetros:
+    - Id del departamento 
+    - nombre del departamento
+    """
+    def __init__(self,departamento_ID,departamento):
+        self.departamento_ID=departamento_ID
+        self.departamento=departamento
+class Ciudad():
+    """
+    Clase ciudad 
+    Parámetros:
+    - Id de la ciudad 
+    - nombre de la ciudad
+    """
+    def __init__(self,ciudad_ID,nombre):
+        self.ciudad_ID=ciudad_ID
+        self.nombre=nombre
 class PadreCategoria:
+    """
+    Clase PadreCategoria 
+    Parámetros:
+    - Id de la categoria padre 
+    - nombre de la categoria padre 
+    """
     def __init__(self, id, nombre):
         self.id=id
         self.nombre=nombre
 class Categoria:
+    """
+    Clase Categoria 
+    Parámetros:
+    - Id de la categoria  
+    - nombre de la categoria  
+    """
     def __init__(self,id,nombre,idPadre):
         self.id=id
         self.nombre=nombre
         self.idPadre=idPadre
-
+class Pago_Domiciliario():
+    """
+    Clase Pago domiciliario 
+    Parámetros:
+    - Id del pago  
+    - estado 
+    - monto a pagar
+    - Id del domiciliario 
+    - Id del financiero
+    """
+    def __init__(self,Pago_domiciliario_ID,estado,monto,domiciliario_ID,financiero_ID):
+        self.Pago_domiciliario_ID=Pago_domiciliario_ID
+        self.estado
+        self.monto
+        self.domiciliario_ID=domiciliario_ID
+        self.financiero_ID=financiero_ID
+class Empaque():
+    """
+    Clase Empaque 
+    Parámetros:
+    - Id del empaque 
+    - Id de la orden de venta 
+    - Id del motivo
+    - Id del usuario 
+    - numero de prendas llevadas 
+    - estado
+    - observaciones
+    """
+    def __init__(self,empaque_ID,orden_venta_ID,motivo_ID,usuario_ID,numero_prendas,estado,observaciones):
+        self.empaque_ID=empaque_ID
+        self.orden_venta_ID=orden_venta_ID
+        self.motivo_ID=motivo_ID
+        self.usuario_ID=usuario_ID
+        self.numero_prendas=numero_prendas
+        self.estado=estado
+        self.observaciones=observaciones
 class Inventario:
     """
     Clase inventario
