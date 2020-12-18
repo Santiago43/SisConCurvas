04aed716653f46cc60d7d41c68de08d8fbd14d9c
BuildTools <santipego0001@gmail.com>
2020-12-18 18:55:44 -0500

    Se agregan las funciones de consultar, actualizar y eliminar y se agregan excepciones a crear en RolesDao.py

--- dao/RolesDao.py
+++ dao/RolesDao.py
@@ -3,8 +3,57 @@ from mysql.connector import errorcode
 from dao.dao import dao
 class RolesDao(dao):
     def crearRol(self,rol):
-        sql= "insert into rol (Nombre) values (%s);"
-        cnx=super().connectDB()
-        cursor=cnx.cursor()
-        cursor.execute(sql,(rol.nombre))
-        return True
+        """
+        """
+        try:
+            sql= "insert into rol (Nombre) values (%s);"
+            cnx=super().connectDB()
+            cursor=cnx.cursor()
+            cursor.execute(sql,(rol.nombre))
+            return True
+        except Exception as e:
+            raise e
+
+    def consultarRol(self,id):
+        """
+        """
+        try:
+            sql= "select * from rol where Rol_ID=%s;"
+            cnx=super().connectDB()
+            cursor=cnx.cursor()
+            cursor.execute(sql,(id))
+        except Exception as e:
+            raise e
+
+    def actualizarRol(self,rol):
+        """
+
+        """
+        try:
+            sql = 'update rol set Nombre=%s where Rol_ID = %s;'
+            cnx=super().connectDB()
+            cursor=cnx.cursor()
+            cursor.execute(sql,(rol.nombre,rol.id))
+        except Exception as e:
+            raise e
+
+    def eliminarRol(self,rol):
+        try:
+            sql="delete from rol where Rol_ID=%s;"
+            cnx=super().connectDB()
+            cursor=cnx.cursor()
+            cursor.execute(sql,(rol.id))
+        except Exception as e:
+            raise e
+
+    def agregarPermiso(self, rol, permiso):
+        try:
+            sql=''
+        except Exception as e:
+            raise e
+        
+    def removerPermiso(self, rol, permiso):
+        try:
+            sql=''
+        except Exception as e:
+            raise e
