5ff73f59fed7a6a2250a7b7ed2b14dea11cdaa7b
BuildTools <santipego0001@gmail.com>
2020-12-19 12:28:26 -0500

    se agrega función para cerrar la conexión de los dao en dao.py, se agrega y retira permisos en RolesDao.py y se agrega UsuariosDao.py

--- dao/RolesDao.py
+++ dao/RolesDao.py
@@ -1,59 +1,107 @@
 import mysql.connector
 from mysql.connector import errorcode
 from dao.dao import dao
+from dao.models import Rol
+from dao.models import Permiso
 class RolesDao(dao):
+    """
+    Clase de objeto de acceso a datos que maneja los roles y sus permisos
+    """
     def crearRol(self,rol):
         """
+        Método que permite hacer el registro de un rol
+        Parámetros:
+        - rol : que es el rol que se registrará 
         """
         try:
             sql= "insert into rol (Nombre) values (%s);"
             cnx=super().connectDB()
             cursor=cnx.cursor()
             cursor.execute(sql,(rol.nombre))
+            super().cerrarConexion(cursor,cnx)
             return True
         except Exception as e:
             raise e
 
     def consultarRol(self,id):
         """
+        Método que permite consultar un rol mediante su ID
+        Parámetros:
+        - id : que es el ID de rol 
         """
         try:
             sql= "select * from rol where Rol_ID=%s;"
             cnx=super().connectDB()
             cursor=cnx.cursor()
             cursor.execute(sql,(id))
+            result = cursor.fetchone()
+            rol = Rol(result[0],result[1],None)
+            sql2='select p.* from rol_tiene_permiso as rp inner join Rol as r on r.Rol_ID=rp.Rol_ID inner join Permiso as p on p.Permiso_ID=rp.Permiso_ID where r.Rol_ID=%s;'
+            cursor.execute(sql2,(id))
+            for row in cursor:
+                rol.permisos.append(Permiso(row[0],row[1]))
+            return rol
+            super().cerrarConexion(cursor,cnx)
         except Exception as e:
             raise e
 
     def actualizarRol(self,rol):
         """
-
+        Método que permite actualizar un rol (su nombre)
+        Parámetros:
+        - rol : que es el rol que se actualizará
         """
         try:
             sql = 'update rol set Nombre=%s where Rol_ID = %s;'
             cnx=super().connectDB()
             cursor=cnx.cursor()
-            cursor.execute(sql,(rol.nombre,rol.id))
+            cursor.execute(sql,(rol.nombre,rol.idRol))
+            super().cerrarConexion(cursor,cnx)
+            return True
         except Exception as e:
             raise e
 
     def eliminarRol(self,rol):
+        """
+        Método que permite eliminar un rol mediante su id
+        - rol : que es el rol que se elinará
+        """
         try:
             sql="delete from rol where Rol_ID=%s;"
             cnx=super().connectDB()
             cursor=cnx.cursor()
-            cursor.execute(sql,(rol.id))
+            cursor.execute(sql,(rol.idRol))
+            cursor.commit()
+            super().cerrarConexion(cursor,cnx)
+            return True
         except Exception as e:
             raise e
 
     def agregarPermiso(self, rol, permiso):
+        """
+        Método que permite agregar permiso a un rol
+        - rol : que es el rol al que se le agregará el permiso
+        - permiso : que es el permiso que se le agregará al rol
+        """
         try:
-            sql=''
+            sql='insert into rol_tiene_permiso (Rol_ID,Permiso_ID) values (%s,%s);'
+            cnx=super().connectDB()
+            cursor=cnx.cursor()
+            cursor.execute(sql,(rol.id,permiso.id))
+            cursor.commit()
+            super().cerrarConexion(cursor,cnx)
+            return True
         except Exception as e:
             raise e
         
     def removerPermiso(self, rol, permiso):
         try:
-            sql=''
+            sql='delete from rol_tiene_permiso where (Rol_ID,Permiso_ID) =(%s,%s);'
+            cnx=super().connectDB()
+            cursor=cnx.cursor()
+            cursor.execute(sql,(rol.id,permiso.id))
+            cursor.commit()
+            super().cerrarConexion(cursor,cnx)
+            return True
         except Exception as e:
             raise e
