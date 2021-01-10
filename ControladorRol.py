816df6bec1583030ae537b946d20f9107500030f
BuildTools <santipego0001@gmail.com>
2021-01-10 17:47:28 -0500

    Se realiza una corrección en este controlador

--- ControladorRol.py
+++ ControladorRol.py
@@ -1,7 +1,18 @@
 from dao.models import Rol
-def crearRol(dao,data,response_object):
+from dao.RolesDao import RolesDao
+def crearRol(data,response_object):
+    """
+    Función que permite crear roles
+
+    Parámetros:
+    
+    - data: que son los datos que vienen de la vista
+    - response_object: que es una referencia a la respuesta del servidor
+
+    Retorna el response_object modificado
+    """
     nombreRol=data.get('nombreRol')
-        
+    dao=RolesDao()
     rol=Rol(None,nombreRol,[])
     if(dao.consultarRolPorNombre(nombreRol) is None):
         if(dao.crearRol(rol)):
@@ -13,7 +24,15 @@ def crearRol(dao,data,response_object):
         response_object['tipo']="error"
         response_object['mensaje']="Ya existe un rol con ese nombre"
     return response_object
-def consultarRoles(dao, response_object):
+
+def consultarRoles(response_object):
+    """
+    Función que permite consultar todos los roles
+    Parámetros
+    - response_object: que es una referencia a la respuesta del servidor
+    Retorna el response_object modificado
+    """
+    dao=RolesDao()
     roles=dao.consultarRoles()
     rolesJson=list()
     for rol in roles:
@@ -26,4 +45,7 @@ def consultarRoles(dao, response_object):
         rolesJson.append(rolDict)
         print(rolDict)
     response_object['Roles']=rolesJson
+    return response_object
+
+def agregarPermisoARol(data,response_object):
     return response_object
