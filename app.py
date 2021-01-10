93f6347a402f0fc22acd19679435f2c95bef1aac
BuildTools <santipego0001@gmail.com>
2021-01-10 17:47:00 -0500

    Se agrega controlador de Usuarios con petición GET completa y se agrega primera parte de modificación de permisos

--- app.py
+++ app.py
@@ -1,23 +1,67 @@
 from flask import Flask, jsonify, request
-from ControladorRol import crearRol, consultarRoles
+from ControladorRol import crearRol, consultarRoles, agregarPermisoARol
+from ControladorUsuarios import crearUsuario, consultarUsuarios
 
-from dao.RolesDao import RolesDao
-from dao.models import Rol
 app = Flask(__name__)
 
 @app.route("/")
 def main_():
+    """
+    Función principal
+    """
     return "Funciona :D"
 
-@app.route("/rol")
+@app.route("/rol", methods=['POST','GET'])
 def roles():
+    """
+    Controlador de roles
+    """
     response_object = {'tipo': 'OK'}
-    dao=RolesDao()
     if request.method=="POST":
         data=request.get_json()
-        response_object=crearRol(dao,data,response_object)  
+        response_object=crearRol(data,response_object)  
     elif request.method=="GET":
-        response_object=consultarRoles(dao,response_object)  
+        response_object=consultarRoles(response_object)  
+    return jsonify(response_object)
+
+@app.route("/rol/permiso",methods=['POST','GET','DELETE'])
+def permisosRoles():
+    """
+    Manejo de permisos de roles
+    """
+    response_object = {'tipo': 'OK'}
+    if request.method=='POST':
+        data=request.get_json()
+        response_object=agregarPermisoARol(data,response_object)
+    elif request.method=='GET':
+        response_object['ans']="Funciona :D"
+    elif request.method=='DELETE':
+        response_object['ans']="Se borró"
+    return jsonify(response_object)
+
+@app.route("/usuario", methods=['POST','GET'])
+def usuarios():
+    """
+    Controlador de usuarios
+    """
+    response_object = {'tipo': 'OK'}
+    if request.method=="POST":
+        data=request.get_json()
+        response_object=crearUsuario(data,response_object)  
+    elif request.method=="GET":
+        response_object=consultarUsuarios(response_object)  
+    return jsonify(response_object)
+@app.route("/orden",methods=['POST','GET'])
+def ordenVenta():
+    """
+    Controlador de órdenes de venta
+    """
+    response_object = {'tipo': 'OK'}
+    if request.method=="POST":
+        data=request.get_json()
+        response_object=crearUsuario(data,response_object)  
+    elif request.method=="GET":
+        response_object=consultarUsuarios(response_object)  
     return jsonify(response_object)
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True)
