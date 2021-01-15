3a5e7a39d80ca59552872cace898b79db0cf92bb
BuildTools <santipego0001@gmail.com>
2021-01-15 17:37:54 -0500

    Se agregan funciones para actualizar y eliminar clientes

--- ControladorClientes.py
+++ ControladorClientes.py
@@ -56,3 +56,73 @@ def consultarClientes(response_object):
         clientesJson.append(clienteDict)
     response_object['clientes']=clientesJson
     return response_object
+
+def actualizarCliente(data,response_object,telefono):
+    """
+    Función que permite actualizar los datos de un cliente a partir de su número telefónico
+
+    Parámetros:
+
+    - data: que son los datos que vienen de la vista
+    - response_object: que es una referencia a la respuesta del servidor
+    - telefono: que es el número de teléfono del cliente
+
+    Retorna el response_object modificado
+    """
+    dao=ClienteDao()
+    cliente=dao.consultarCliente(telefono)
+    if cliente is not None:
+        primerNombre=data.get('primerNombre')
+        if primerNombre is not None:
+            cliente.primerNombre=primerNombre
+        segundoNombre=data.get('segundoNombre')
+        if segundoNombre is not None:
+            cliente.segundonombre=segundoNombre
+        primerApellido=data.get('primerApellido')
+        if primerApellido is not None:
+            cliente.primerApellido=primerApellido
+        segundoApellido=data.get('segundoApellido')
+        if segundoApellido is not None:
+            cliente.segundoApellido=segundoApellido
+        correo=data.get('correo')
+        if correo is not None:
+            cliente.correo=correo
+        tipoCliente=data.get('tipoCliente')
+        if tipoCliente is not None:
+            cliente.tipoCliente=tipoCliente
+        if dao.actualizarCliente(cliente):
+            response_object['mensaje']="Cliente actualizado"
+        else:
+            response_object['tipo']="error"
+            response_object['mensaje']="Error al actualizar cliente"
+    else:
+        response_object['tipo']="error"
+        response_object['mensaje']="No existe un cliente con ese número telefónico"
+    return response_object
+
+def eliminarCliente(response_object,telefono):
+    """
+    Función que permite eliminar los datos de un cliente a partir de su número telefónico.
+    Primero realiza la consulta del cliente para posteriormente eliminarlo.
+
+    Parámetros:
+
+    - response_object: que es una referencia a la respuesta del servidor
+    - telefono: que es el número de teléfono del cliente
+    
+    Retorna el response_object modificado
+    """
+    dao=ClienteDao()
+    cliente=dao.consultarCliente(telefono)
+    if cliente is not None:
+        if dao.eliminarCliente(cliente):
+            response_object['mensaje']="Cliente eliminado"
+        else:
+            response_object['tipo']="error"
+            response_object['mensaje']="Error al eliminar cliente"
+    else:
+        response_object['tipo']="error"
+        response_object['mensaje']="No existe un cliente con ese número telefónico"
+    return response_object
+    
+    
