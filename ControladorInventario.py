17782d4683fd07e4a9a8e90ad59c9e38bb6856e7
BuildTools <santipego0001@gmail.com>
2021-01-16 14:57:33 -0500

    Se agrega actualizar y eliminar productos

--- ControladorInventario.py
+++ ControladorInventario.py
@@ -35,7 +35,8 @@ def consultarProductos(response_object):
     Función que permite consultar todos los productos. 
 
     Esta opera de tal forma que convierte los datos de la base de datos en objetos 
-    y luego los convierte en diccionarios. 
+    y luego los convierte en diccionarios.
+    
     Parámetros:
     - response_object: que es una referencia a la respuesta del servidor
     Retorna el response_object modificado
@@ -51,4 +52,66 @@ def consultarProductos(response_object):
         productoDict['categorias']=categoriasDict
         productosDict.append(productoDict)
     response_object['productos']=productosDict
+    return response_object
+
+def actualizarProducto(data,response_object,referencia):
+    """
+    Función que permite actualizar un producto mediante su referencia. 
+
+    Parámetros:
+    - data: data: que son los datos que vienen de la vista
+    - response_object: que es una referencia a la respuesta del servidor
+    
+    Retorna el response_object modificado
+    """
+    dao = InventarioDao()
+    producto=dao.consultarProducto(referencia)
+    if producto is not None:
+        descripcion = data.get("descripcion")
+        if descripcion is not None:
+            producto.descripcion=descripcion
+        urlImagen = data.get("urlImagen")
+        if urlImagen is not None:
+            producto.urlImagen=urlImagen
+        stock = data.get("stock")
+        if stock is not None:
+            producto.stock=stock
+        precioCosto=data.get("precioCosto")
+        if precioCosto is not None:
+            producto.precioCosto=precioCosto
+        precioVenta=data.get("precioVenta")
+        if precioVenta is not None:
+            producto.precioVenta=precioVenta
+        if dao.actualizarProducto(producto):
+            response_object['mensaje']="Producto actualizado"
+        else:
+            response_object['tipo']="error"
+            response_object['mensaje']="Error al actualizar producto"
+    else:
+        response_object['tipo']="error"
+        response_object['mensaje']="No existe un producto con esa referencia"
+    return response_object
+
+
+def eliminarProducto(response_object,referencia):
+    """
+    Función que permite actualizar un producto mediante su referencia. 
+
+    Parámetros:
+    - data: data: que son los datos que vienen de la vista
+    - response_object: que es una referencia a la respuesta del servidor
+    
+    Retorna el response_object modificado
+    """
+    dao = InventarioDao()
+    producto=dao.consultarProducto(referencia)
+    if producto is not None:
+        if dao.eliminarproducto(producto):
+            response_object['mensaje']="Producto eliminado"
+        else:
+            response_object['tipo']="error"
+            response_object['mensaje']="Error al eliminar producto"
+    else:
+        response_object['tipo']="error"
+        response_object['mensaje']="No existe un producto con esa referencia"
     return response_object
