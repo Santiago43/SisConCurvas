784e8086bede8b910ee74766ecb9b93cfb5ae393
unknown <santiago.gutierrez02@correo.usa.edu.co>
2021-01-16 14:27:30 -0500

    PUT and DELETE Categoria

--- ControladorCategorias.py
+++ ControladorCategorias.py
@@ -41,3 +41,38 @@ def consultarCategorias(response_object):
         categoriasDict.append(categoria.__dict__)
     response_object['categorias']=categoriasDict
     return response_object
+
+def actualizarcategoria(response_object):
+    """
+    Función que permite consultar todos los categorias. 
+
+    Esta opera de tal forma que convierte los datos de la base de datos en objetos 
+    y luego los convierte en diccionarios. 
+    Parámetros:
+    - response_object: que es una referencia a la respuesta del servidor
+    Retorna el response_object modificado
+    """
+    dao = CategoriaDao()
+
+def eliminarcategoria(response_object, categoria_ID):
+    """
+    Función que permite consultar todos los categorias. 
+
+    Esta opera de tal forma que convierte los datos de la base de datos en objetos 
+    y luego los convierte en diccionarios. 
+    Parámetros:
+    - response_object: que es una referencia a la respuesta del servidor
+    Retorna el response_object modificado
+    """
+    dao = CategoriaDao()
+    categoria = dao.consultarCategoria(categoria_ID)
+    if categoria is not None:
+        if dao.eliminarcategoria(categoria):
+            response_object['mensaje']="Categoria eliminada"
+        else:
+            response_object['tipo']="error"
+            response_object['mensaje']="Error al eliminar categoria"
+    else:
+        response_object['tipo']="error"
+        response_object['mensaje']="No existe una categoria con ese número telefónico"
+    return response_object
