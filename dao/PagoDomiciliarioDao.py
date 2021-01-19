37303511fd34167df8e0e3b5f2c119e6acd591aa
BuildTools <santipego0001@gmail.com>
2021-01-19 11:45:30 -0500

    Se agrega Dao de pagos de domiciliario

--- dao/PagoDomiciliarioDao.py
+++ dao/PagoDomiciliarioDao.py
@@ -16,7 +16,7 @@ class PagoDomiciliarioDao(dao):
         cnx=super().connectDB()
         cursor=cnx.cursor()
         try:
-            args=[pago.monto,pago.domiciliario_ID,pago.financiero_ID]
+            args=[pago.monto,pago.domiciliario_ID,pago.financiero_ID,pago.estado]
             cursor.callproc("insertarPagoDomiciliario",args)
             cnx.commit()
             super().cerrarConexion(cursor,cnx)
@@ -28,18 +28,85 @@ class PagoDomiciliarioDao(dao):
         """
         Método que permite hacer la consulta de un pago mediante su id
         """
+        cnx=super().connectDB()
+        cursor=cnx.cursor()
         try:
-            sql= 'select * from Pago_domiciliario where Pago_domiciliario_id='+str(id)+';'
+            sql= '''select pd.*,dp.Usuario_ID as Domiciliario_ID,fp.Usuario_ID as Financiero_ID from Pago_domiciliario as pd
+            inner join Domiciliario_tiene_Pago as dp on dp.Pago_domiciliario_ID=pd.Pago_domiciliario_ID
+            inner join Financiero_hace_pago as fp on pd.Pago_domiciliario_ID=fp.Pago_domiciliario_ID
+            where pd.Pago_domiciliario_ID=%s;'''
+            cursor.execute(sql,(id,))
+            result=cursor.fetchone()
+            pago=None
+            if result is not None:
+                pago=PagoDomiciliario(result[0],result[1],result[2],result[3],result[4],result[5])
+            super().cerrarConexion(cursor,cnx)
+            return pago
+        except Exception as e:
+            super().cerrarConexion(cursor,cnx)
+            raise e
+    def actualizarPago(self,pago):
+        """
+        Método que permite actualizar un pago
+        Parámetros:
+        - pago : que es el pago que se actualizará
+        """
+        try:
+            sql = '''update Pago_domiciliario as pd set
+            pd.Estado=%s,
+            pd.Monto=%s,
+            pd.fecha_pago=cast(sysdate() as date)
+            where pd.Pago_domiciliario_ID=%s;'''
             cnx=super().connectDB()
             cursor=cnx.cursor()
-            cursor.execute(sql)
-            result=cursor.fetchone()
-            if result is not None:
-            motivos=list()
-            for result in results:
-                motivo=Motivo(result[0],result[1],result[2])
-                motivos.append(motivo)
+            args=(pago.estado,pago.monto,pago.pagoDomiciliario_ID)
+            cursor.execute(sql,args)
+            cnx.commit()
             super().cerrarConexion(cursor,cnx)
-            return motivos
-        except Exception as e:
+            return True
+        except Exception as e:            
+            super().cerrarConexion(cursor,cnx)
+            raise e
+    def eliminarPago(self,pago):
+        """
+        Método que permite actualizar un pago
+        Parámetros:
+        - pago : que es el pago que se actualizará
+        """
+        try:
+            sql = '''update Pago_domiciliario as pd set
+            pd.Estado=%s,
+            pd.Monto=%s,
+            pd.fecha_pago=cast(sysdate() as date)
+            where pd.Pago_domiciliario_ID=%s;'''
+            cnx=super().connectDB()
+            cursor=cnx.cursor()
+            args=(pago.estado,pago.monto,pago.pagoDomiciliario_ID)
+            cursor.execute(sql,args)
+            cnx.commit()
+            super().cerrarConexion(cursor,cnx)
+            return True
+        except Exception as e:            
+            super().cerrarConexion(cursor,cnx)
+            raise e
+    def consultarPagos(self):
+        """
+        Método que permite hacer la consulta de todos los pagos
+        """
+        cnx=super().connectDB()
+        cursor=cnx.cursor()
+        try:
+            sql= '''select pd.*,dp.Usuario_ID as Domiciliario_ID,fp.Usuario_ID as Financiero_ID from Pago_domiciliario as pd
+            inner join Domiciliario_tiene_Pago as dp on dp.Pago_domiciliario_ID=pd.Pago_domiciliario_ID
+            inner join Financiero_hace_pago as fp on pd.Pago_domiciliario_ID=fp.Pago_domiciliario_ID;'''
+            cursor.execute(sql)
+            results=cursor.fetchall()
+            pagos=list()
+            for result in results:
+                pago=PagoDomiciliario(result[0],result[1],result[2],result[3],result[4],result[5])
+                pagos.append(pago)
+            super().cerrarConexion(cursor,cnx)
+            return pagos
+        except Exception as e:            
+            super().cerrarConexion(cursor,cnx)
             raise e
