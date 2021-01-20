6fd2078e60457c647915ad44ba14b0fa91eb7867
Santiago43 <santipego0001@gmail.com>
2021-01-19 19:22:16 -0500

    Se agrega control de inventario para observar quién creó y editó productos

--- ControladorInventario.py
+++ ControladorInventario.py
@@ -1,7 +1,8 @@
-from dao.models import Inventario
-from dao.models import Categoria
+from dao.models import Inventario,Categoria,Control_inventario
 from dao.InventarioDao import InventarioDao
-def crearProducto(data,response_object):
+from dao.ControlDao import ControlDao
+from dao.CategoriaDao import CategoriaDao
+def crearProducto(data,response_object,usuario):
     """
     Función que permite crear productos
     Parámetros
@@ -22,6 +23,14 @@ def crearProducto(data,response_object):
     if(dao.consultarProducto(referenciaProducto) is None):
         if(dao.crearProducto(producto)):
             response_object['mensaje']="producto creado"
+            categoriaDao=CategoriaDao()
+            for categoria in categorias:
+                print(categoria)
+                categoriaObj=categoriaDao.consultarCategoria(categoria['id'])
+                dao.agregarCategoria(producto,categoriaObj)
+            control=Control_inventario(usuario.usuario_ID,None,referenciaProducto,None,0,"Se crea: "+descripcion,stock,True)
+            controlDao=ControlDao()
+            controlDao.crearControlInventario(control)
         else:
             response_object['tipo']="error"
             response_object['mensaje']="Error al crear producto"
@@ -54,7 +63,7 @@ def consultarProductos(response_object):
     response_object['productos']=productosDict
     return response_object
 
-def actualizarProducto(data,response_object,referencia):
+def actualizarProducto(data,response_object,referencia,usuario):
     """
     Función que permite actualizar un producto mediante su referencia. 
 
@@ -67,23 +76,32 @@ def actualizarProducto(data,response_object,referencia):
     dao = InventarioDao()
     producto=dao.consultarProducto(referencia)
     if producto is not None:
+        camposEditados=""
         descripcion = data.get("descripcion")
         if descripcion is not None:
+            camposEditados+="Descripción, "
             producto.descripcion=descripcion
         urlImagen = data.get("urlImagen")
         if urlImagen is not None:
+            camposEditados+="Imagen, "
             producto.urlImagen=urlImagen
         stock = data.get("stock")
         if stock is not None:
+            camposEditados+="Stock, "
             producto.stock=stock
         precioCosto=data.get("precioCosto")
         if precioCosto is not None:
+            camposEditados+="Precio costo, "
             producto.precioCosto=precioCosto
         precioVenta=data.get("precioVenta")
         if precioVenta is not None:
+            camposEditados+="Precio venta"
             producto.precioVenta=precioVenta
         if dao.actualizarProducto(producto):
             response_object['mensaje']="Producto actualizado"
+            control = Control_inventario(usuario.usuario_ID,None,producto.referenciaProducto,None,producto.stock,"El usuario "+usuario.primerNombre+" "+usuario.primerApellido+ " editó los campos: "+camposEditados,0,False)
+            controlDao=ControlDao()
+            controlDao.crearControlInventario(control)
         else:
             response_object['tipo']="error"
             response_object['mensaje']="Error al actualizar producto"
@@ -111,6 +129,77 @@ def eliminarProducto(response_object,referencia):
         else:
             response_object['tipo']="error"
             response_object['mensaje']="Error al eliminar producto"
+    else:
+        response_object['tipo']="error"
+        response_object['mensaje']="No existe un producto con esa referencia"
+    return response_object
+
+
+def agregarStock(data,response_object,referencia,usuario):
+    """
+    Función que permite actualizar un el stock de producto mediante su referencia. 
+
+    Parámetros:
+    - data: data: que son los datos que vienen de la vista
+    - response_object: que es una referencia a la respuesta del servidor
+    
+    Retorna el response_object modificado
+    """
+    dao = InventarioDao()
+    producto=dao.consultarProducto(referencia)
+    stock=data.get('stock')
+    if producto is not None:
+        if stock is not None:
+            producto.stock+=stock
+            if dao.actualizarProducto(producto):
+                response_object['mensaje']="Producto actualizado"
+                control = Control_inventario(usuario.usuario_ID,None,referencia,None,producto.stock-stock,"El usuario "+usuario.primerNombre+" "+usuario.segundoNombre+" agregó al inventario "+str(stock)+ " del producto '"+producto.referenciaProducto+"'",stock,True)
+                controlDao=ControlDao()
+                controlDao.crearControlInventario(control)
+            else:
+                response_object['tipo']="error"
+                response_object['mensaje']="Error al actualizar producto"
+        else:
+            response_object['tipo']="error"
+            response_object['mensaje']="Valor inválido de ingreso"
+    else:
+        response_object['tipo']="error"
+        response_object['mensaje']="No existe un producto con esa referencia"
+    return response_object
+
+
+def retirarStock(data,response_object,referencia,usuario):
+    """
+    Función que permite actualizar un el stock de producto mediante su referencia.
+
+    Parámetros:
+    
+    - data: data: que son los datos que vienen de la vista
+    - response_object: que es una referencia a la respuesta del servidor
+    
+    Retorna el response_object modificado
+    """
+    dao = InventarioDao()
+    producto=dao.consultarProducto(referencia)
+    if producto is not None:
+        stock = data.get("stock")
+        if stock is not None:
+            if stock >producto.stock:
+                response_object['tipo']="error"
+                response_object['mensaje']="No puede retirar más de la cantidad presente en el inventario. Actualmente hay "+str(producto.stock)+" unidad(es)"
+            else:
+                producto.stock-=stock
+                if dao.actualizarProducto(producto):
+                    response_object['mensaje']="Stock actualizado"
+                    control=Control_inventario(usuario.usuario_ID,None,referencia,None,producto.stock+stock,"El usuario "+usuario.primerNombre+" "+usuario.segundoNombre+" retiró del inventario "+str(stock)+ " del producto '"+producto.referenciaProducto+"'",stock,False)
+                    controlDao=ControlDao()
+                    controlDao.crearControlInventario(control)
+                else:
+                    response_object['tipo']="error"
+                    response_object['mensaje']="Error al actualizar producto"
+        else:
+            response_object['tipo']="error"
+            response_object['mensaje']="Valor inválido de retiro"
     else:
         response_object['tipo']="error"
         response_object['mensaje']="No existe un producto con esa referencia"
