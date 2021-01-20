494cfa8380d1123eb4343d7cdeb1aec5ed6bd625
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2021-01-19 19:37:12 -0500

    Se agregan controles de rol y permisos de rol

--- ControladorRol.py
+++ ControladorRol.py
@@ -2,6 +2,7 @@ from dao.models import Rol,Control_rol
 from dao.RolesDao import RolesDao
 from dao.PermisoDao import PermisoDao
 from dao.ControlDao import ControlDao
+
 def crearRol(data,response_object,usuario):
     """
     Función que permite crear roles
@@ -65,6 +66,7 @@ def actualizarRol(data,response_object,rol_ID,editor):
     - data: que son los datos que vienen de la vista
     - response_object: que es una referencia a la respuesta del servidor
     - rol_ID: que es el id del rol que se quiere actualizar
+    - editor: Usuario que realiza la acción
 
     Retorna el response_object modificado
     """
@@ -76,6 +78,11 @@ def actualizarRol(data,response_object,rol_ID,editor):
             rol.nombre=nombre
         if dao.actualizarRol(rol):
             response_object['mensaje']="Rol actualizado"
+            rol=dao.consultarRolPorNombre(nombre)
+            texto="El usuario "+editor.primerNombre+" "+editor.primerApellido+" modificó el rol '"+nombre+"'"
+            control=Control_rol(None,rol.idRol,editor.usuario_ID,None,2,texto)
+            controlDao=ControlDao()
+            controlDao.crearControlRol(control)
         else:
             response_object['tipo']="Error"
             response_object['mensaje']="Error al actualizar el rol"
@@ -91,14 +98,20 @@ def eliminarRol(response_object,rol_ID,editor):
     Parámetros:
     - response_object: que es una referencia a la respuesta del servidor
     - rol_ID: que es el id del rol que se quiere actualizar
+    - editor: Usuario que realiza la acción
 
     Retorna el response_object modificado
     """
     dao=RolesDao()
     rol=dao.consultarRol(rol_ID)
+    nombreRol = rol.nombre
     if rol is not None:
         if dao.eliminarRol(rol):
             response_object['mensaje']="Rol eliminado"
+            texto="El usuario "+editor.primerNombre+" "+editor.primerApellido+" eliminó el rol '"+nombreRol+"'"
+            control=Control_rol(None,rol_ID,editor.usuario_ID,None,3,texto)
+            controlDao=ControlDao()
+            controlDao.crearControlRol(control)
         else:
             response_object['tipo']="Error"
             response_object['mensaje']="Error al eliminar el rol"
@@ -107,7 +120,18 @@ def eliminarRol(response_object,rol_ID,editor):
         response_object['mensaje']="No existe un rol con ese ID"
     return response_object
 
-def agregarPermisoARol(response_object,rol_ID,permiso_ID):
+def agregarPermisoARol(response_object,rol_ID,permiso_ID,editor):
+    """
+    Función que permite agregar un permiso a un rol
+
+    Parámetros:
+    - response_object: referencia a la respuesta del servidor
+    - rol_ID: Id del rol al que se quiere agregar el permiso
+    - permiso_ID: Permiso que se desee agregar
+    - editor: Usuario que realiza la acción
+
+    Retorna el response_object modificado
+    """
     permisoDao=PermisoDao()
     rolDao=RolesDao()
     permiso = permisoDao.consultarPermiso(permiso_ID)
@@ -115,6 +139,10 @@ def agregarPermisoARol(response_object,rol_ID,permiso_ID):
     if permiso and rol is not None:
         if rolDao.agregarPermiso(rol,permiso):
             response_object['mensaje']="Permiso '"+permiso.nombre+"' agregado al rol "+rol.nombre
+            texto="El usuario "+editor.primerNombre+" "+editor.primerApellido+" agregó el permiso '"+permiso.nombre+"' al rol '"+rol.nombre+"'"
+            control=Control_rol(None,rol.idRol,editor.usuario_ID,None,1,texto)
+            controlDao=ControlDao()
+            controlDao.crearControlRol(control)
         else:
             response_object['tipo']="error"
             response_object['mensaje']="Error al asignar permiso"
@@ -123,7 +151,18 @@ def agregarPermisoARol(response_object,rol_ID,permiso_ID):
         response_object['mensaje']="No existe ese permiso o ese rol"
     return response_object
 
-def removerPermisoARol(response_object,rol_ID,permiso_ID):
+def removerPermisoARol(response_object,rol_ID,permiso_ID, editor):
+    """
+    Función que permite remover un permiso a un rol
+
+    Parámetros:
+    - response_object: referencia a la respuesta del servidor
+    - rol_ID: Id del rol al que se quiere eliminar el permiso
+    - permiso_ID: Permiso que se desee remover
+    - editor: Usuario que realiza la acción
+
+    Retorna el response_object modificado
+    """
     permisoDao=PermisoDao()
     rolDao=RolesDao()
     permiso = permisoDao.consultarPermiso(permiso_ID)
@@ -131,6 +170,10 @@ def removerPermisoARol(response_object,rol_ID,permiso_ID):
     if permiso and rol is not None:
         if rolDao.removerPermiso(rol,permiso):
             response_object['mensaje']="Permiso '"+permiso.nombre+"' removido del rol "+rol.nombre
+            texto="El usuario "+editor.primerNombre+" "+editor.primerApellido+" removió el permiso '"+permiso.nombre+"' del rol '"+rol.nombre+"'"
+            control=Control_rol(None,rol.idRol,editor.usuario_ID,None,3,texto)
+            controlDao=ControlDao()
+            controlDao.crearControlRol(control)
         else:
             response_object['tipo']="error"
             response_object['mensaje']="Error al asignar permiso. Probablemente ya tenga este permiso."
