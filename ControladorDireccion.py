4850e1514a943ca9edc817724e6aefe65420f431
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2021-01-21 19:16:53 -0500

    Se crea el controlador Direccion

---
+++ ControladorDireccion.py
@@ -0,0 +1,17 @@
+from dao.models import Direccion
+from dao.DireccionDao import DireccionDao
+
+def consultarDireccion(response_object, direccionID):
+    """
+    Función que permite consultar las direcciones. 
+
+    Esta opera de tal forma que convierte los datos de la base de datos en objetos 
+    y luego los convierte en diccionarios. 
+    Parámetros:
+    - response_object: que es una referencia a la respuesta del servidor
+    Retorna el response_object modificado
+    """
+    dao = DireccionDao()
+    direccion=dao.consultarDireccion(direccionID)
+    response_object['direccion']=direccion
+    return response_object
