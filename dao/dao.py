1cfcff5e7b4ef346903084fe44ef961d75cc4a86
Santiago43 <santipego0001@gmail.com>
2021-01-25 16:40:04 -0500

    Se cambian nombre de usuario y nombre de base de datos

--- dao/dao.py
+++ dao/dao.py
@@ -5,9 +5,9 @@ class dao:
     docstring
     """
     def __init__(self):
-        self.user='concurvas'
+        self.user='concurva_sis'
         self.password='XTVwTewvQp2cTfC'
-        self.database='concurvas'
+        self.database='concurva_sistema'
         self.host='127.0.0.1'
     def connectDB(self):
         cnx = mysql.connector.connect(user=self.user, password = self.password, database=self.database, host=self.host)
